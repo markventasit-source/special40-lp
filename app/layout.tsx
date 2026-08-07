0cea7d4b4713b5f5f41b388274274d2dcddb124a
markventasit-source <markventas.it@gmail.com>
2026-08-07 15:03:55 +0530

    added gtag page view

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,7 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-18354990280');
+            gtag('config', 'G-KQ0C3M286J');
           `}
         </Script>
 
